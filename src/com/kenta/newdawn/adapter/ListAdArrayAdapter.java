5581b5545d7ad2b2a1249b11f447202915666e30
Kamol Mavlonov <kamol@701search.com>
2013-04-18 21:47:57 +0800

    Added performace optimization feature for ListAdArrayAdapter

--- src/com/kenta/newdawn/adapter/ListAdArrayAdapter.java
+++ src/com/kenta/newdawn/adapter/ListAdArrayAdapter.java
@@ -16,6 +16,7 @@ public class ListAdArrayAdapter extends ArrayAdapter<Ad> {
 
     private ImageTagFactory imageTagFactory = ImageTagFactory.newInstance();
 
+    ViewHolder viewHolder = null;
 
     public ListAdArrayAdapter(Context context) {
         super(context, 0);
@@ -24,25 +25,39 @@ public class ListAdArrayAdapter extends ArrayAdapter<Ad> {
         imageTagFactory.setWidth(500);
         imageTagFactory.setDefaultImageResId(R.drawable.no_image);
         imageTagFactory.setErrorImageId(R.drawable.ic_launcher);
-        //imageTagFactory.setAnimation(android.R.anim.slide_in_left);
+        imageTagFactory.setAnimation(android.R.anim.slide_in_left);
         //imageTagFactory.setAnimation(android.R.anim.fade_in);
         imageTagFactory.setSaveThumbnail(true);
     }
 
-    // TODO: where is ViewHolder?
+    /**
+     *
+     * Improve {@link android.widget.ListView} performance while scrolling<br/>
+     *
+     * <a href="ttp://developer.android.com/training/improving-layouts/smooth-scrolling.html#ViewHolder">See doc</a>
+     */
+    static class ViewHolder {
+        ImageView iv;
+        ImageTag it;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_sample, null);
+            viewHolder = new ViewHolder();
+            viewHolder.iv = (ImageView) convertView.findViewById(R.id.item_image);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-        ImageView iv = (ImageView) convertView.findViewById(R.id.item_image);
-        ImageTag tag = imageTagFactory.build(getItem(position).image.replace("thumbs", "images"), getContext());
-        //ImageTag tag = mTagFactory.build(res.getUrl().replace("images", "thumbs"), getContext());
-        iv.setTag(tag);
-        NewDawnApplication.getImageManager().getLoader().load(iv);
+        viewHolder.it = imageTagFactory.build(getItem(position).image.replace("thumbs", "images"), getContext());
+        viewHolder.iv.setTag(viewHolder.it);
+        NewDawnApplication.getImageManager().getLoader().load(viewHolder.iv);
 
         return convertView;
     }
+
 
     @Override
     public Ad getItem(int position) {
